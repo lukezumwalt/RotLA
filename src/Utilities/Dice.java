43926522e3dfbeda95db21096c2d89a93c3f991c
zumwa020 <zumwa020@umn.edu>
2022-09-20 16:54:34 -0600

    fixing minimum possible value on dice roll

--- src/Utilities/Dice.java
+++ src/Utilities/Dice.java
@@ -6,7 +6,7 @@ public class Dice {
     public static int rollD6(int numberOfDice){
         /* https://stackoverflow.com/questions/5271598/java-generate-random-number-between-two-given-values */
         Random r = new Random();
-        int low = 1;
+        int low = numberOfDice;   // smallest possible value is a one on every die rolled, thus #ofDice == minimum roll
         int high = 6*numberOfDice;
         return(r.nextInt(high-low) + low );
     }
